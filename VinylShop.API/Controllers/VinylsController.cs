77abcc6233ae7de7595a136754b93c55d7c90e98
ubtk <--unset>
2026-04-27 14:21:23 +0300

    add model

---
+++ VinylShop.API/Controllers/VinylsController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VinylShop.API.Models;
+
+
+namespace VinylShop.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class VinylsController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<Vinyl>> GetAll()
+        {
+            var vinyls = new List<Vinyl> {
+                new Vinyl
+                {
+                    Id = 1,
+                    Title = "Good kid, mad city",
+                    Artist = "Kendrick Lamar",
+                    Year = 2012,
+                    Price = 67.52m
+                }
+            };
+
+            return Ok(vinyls);
+        }
+
+    }
+}
+
