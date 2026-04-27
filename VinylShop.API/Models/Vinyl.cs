6c0eb7b681735d2013137258a31bdca15d6d50c2
ubtk <puzanova0708@gmail.com>
2026-04-27 13:31:14 +0300

    Initial commit

---
+++ VinylShop.API/Models/Vinyl.cs
@@ -0,0 +1,11 @@
+﻿namespace VinylShop.API.Models
+{
+    public class Vinyl
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Artist { get; set; }
+        public int Year { get; set; }
+        public decimal Price { get; set; }
+    }
+}
